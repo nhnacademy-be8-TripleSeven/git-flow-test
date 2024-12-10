c12a150e526382633c38d8d6366472cd2bb32f32
wjpark4430 <pwj4na2@gmail.com>
2024-12-10 11:06:29 +0900

    ✅ Test nginx

--- src/main/java/com/nhnacademy/hello/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/index/IndexController.java
@@ -1,12 +1,20 @@
 package com.nhnacademy.hello.index;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
     @GetMapping(value = {"/index.html","/"})
-    public String index(){
-        return "index/index";
+    public String index(HttpServletRequest request, ModelMap model){
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null ){
+            ip = request.getRemoteAddr();
+        }
+        model.addAttribute("ip",ip);
+        return "index";
     }
 }
