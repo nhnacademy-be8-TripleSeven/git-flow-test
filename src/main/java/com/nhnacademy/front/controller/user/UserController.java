e1be98d589ae5e036bc1edfac0c807cac7b55589
lushlife99 <wjdcks282828@gmail.com>
2024-12-18 20:34:38 +0900

    feat : My Page Controller 추가

--- src/main/java/com/nhnacademy/front/controller/user/UserController.java
+++ src/main/java/com/nhnacademy/front/controller/user/UserController.java
@@ -6,14 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("/api/frontend/member/my-page")
+    @GetMapping("/api/frontend/member/my-page/info")
     public String getMyPage() {
-        return "/member/my-page";
+        return "/member/my-page/info";
+    }
+
+    @GetMapping("/frontend/guest/orders/search")
+    public String searchGuestOrder() {
+        return "/guest/order/search";
     }
 
     @GetMapping("/frontend/guest/orders")
-    public String searchGuestOrder() {
-        return "/guest/order-search";
+    public String getGuestOrderDetails() {
+        return "/guest/order/detail";
     }
 
     @GetMapping("/frontend/guest/find/order-number")
@@ -25,4 +30,38 @@ public class UserController {
     public String findGuestOrderPassword() {
         return "/guest/search/order-password";
     }
+
+    @GetMapping("/frontend/member/my-page/order/manage")
+    public String manageMemberOrders() {
+        return "/member/my-page/order/manage";
+    }
+
+    @GetMapping("/frontend/member/my-page/order/refund-exchange")
+    public String getRefundExchangeDetails() {
+        return "/member/my-page/order/refund-exchange";
+    }
+
+    @GetMapping("/frontend/member/my-page/like")
+    public String getMyLikeBooks() {
+        return "/member/my-page/like/like";
+    }
+
+    @GetMapping("/frontend/member/my-page/points/use")
+    public String getMyPointUseHistory() {
+        return "/member/my-page/point-history/use";
+    }
+
+    @GetMapping("/frontend/member/my-page/points/earn")
+    public String getMyPointEarnHistory() {
+        return "/member/my-page/point-history/earn";
+    }
+
+    @GetMapping("/frontend/member/my-page/coupons")
+    public String getCouponDetails() {
+        return "/member/my-page/coupon/detail";
+    }
+
+
+
+
 }
